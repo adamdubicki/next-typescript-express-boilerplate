304344f2049052dab73e7879d56475951cddd108
adamdubicki <adam@freshworks.io>
2019-06-10 08:55:33 -0700

    text commit with prettier
    test commit

--- server/index.ts
+++ server/index.ts
@@ -6,17 +6,19 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.get('*', (req, res) => handle(req, res));
+    server.get('*', (req, res) => handle(req, res));
 
-  server.listen(port, '0.0.0.0', (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    server.listen(port, '0.0.0.0', (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-})
-.catch((ex) => {
-  console.error(ex.stack);
-  process.exit(1);
-});

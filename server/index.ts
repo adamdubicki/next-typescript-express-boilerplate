aa0d5cabc48150083f16ae5b0d9e8600de7fcee1
adamdubicki <adam@freshworks.io>
2019-06-10 08:36:53 -0700

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import next from 'next';
+import express from 'express';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, '0.0.0.0', (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+})
+.catch(ex => {
+  console.error(ex.stack);
+  process.exit(1);
+});

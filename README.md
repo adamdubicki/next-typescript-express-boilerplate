775230566abb4527f46e9bf04d16955fff18be33
adamdubicki <adam@freshworks.io>
2019-06-10 08:55:06 -0700

    adding prettier + lint

---
+++ README.md
@@ -0,0 +1,21 @@
+# Custom server with TypeScript, Express & Nodemon example
+
+## Install it and run:
+
+Dev build
+
+```bash
+npm install
+npm run dev
+# or
+yarn
+yarn dev
+```
+
+Production build
+
+```bash
+npm install
+npm run build
+npm run start
+```
--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import next from 'next';
 import express from 'express';
 
 const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -16,7 +16,7 @@ app.prepare().then(() => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 })
-.catch(ex => {
+.catch((ex) => {
   console.error(ex.stack);
   process.exit(1);
 });
